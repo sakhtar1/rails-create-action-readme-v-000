c2f5b2166d6bbc0836902ada64e875a945ffe06d
Sharmina Akhtar <sakht318@gmail.com>
2018-04-16 13:05:24 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,10 @@ class PostsController < ApplicationController
 	end
 
 	def create
-  post = Post.new
-  post.title = params[:title]
-  post.description = params[:description]
-  post.save
-end
+	  post = Post.new
+	  post.title = params[:title]
+	  post.description = params[:description]
+	  post.save
+  end
+
 end
